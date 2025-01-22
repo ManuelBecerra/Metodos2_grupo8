db4b17d2491c07a9e30fddc16140f3c19cd60e0b
morduzp <m.orduzp@uniandes.edu.co>
2025-01-21 21:57:45 -0500

    Punto 1a

--- tarea 1.py
+++ tarea 1.py
@@ -2,20 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sym
 from scipy.optimize import curve_fit
+from scipy.signal import medfilt
 import pandas as pd
 
+'''Ejercicio 1'''
+
+
+'''1a'''
 data = pd.read_csv("Rhodium.csv")
 x_data = data['Wavelength (pm)']
 y_data = data['Intensity (mJy)']
 
 
-plt.figure(1)
+def filtered_data(data):
+    filtered_intensity = medfilt(data['Intensity (mJy)'], kernel_size=5)
+    corrupt_data_mask = data['Intensity (mJy)'] != filtered_intensity
+    num_corrupt_data = corrupt_data_mask.sum()
+    
+    return filtered_intensity, num_corrupt_data
 
-plt.scatter(x_data, y_data, label="Data")
+y_data_filtered = filtered_data(data)[0]
+
+print(f"1.a) Número de datos eliminados : {filtered_data(data)[1]}")
+plt.plot(x_data, y_data, label="Original data", color = "darkturquoise")
+plt.plot(x_data, y_data_filtered, label="Filtered data", color = "coral")
 plt.xlabel('Wavelength (pm)')
 plt.ylabel('Intensity (mJy)')
 plt.title('Intensity vs Wavelength')
-plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.25), fancybox=True, shadow=True, ncol=1)
+plt.legend()
 plt.tight_layout()
+
+
+output_path = "limpieza.pdf"
+plt.savefig(output_path)
 plt.show()
 
+print(f"1.a) Número de datos eliminados : {filtered_data(data)[1]}")
+
+'''1b'''
