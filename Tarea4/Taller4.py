3983a1dbca04c1af2f810c66a3a8bcbde63e9688
ManuelBecerra <becerramanu21@gmail.com>
2025-03-12 17:59:48 -0500

    punto 1

--- Tarea4/Taller4.py
+++ Tarea4/Taller4.py
@@ -3,6 +3,58 @@ import matplotlib.pyplot as plt
 
 '''Ejercicio 1: Integración indirecta'''
 
+random = np.random.default_rng()
+def g(x, n=10, alpha=4/5):
+    return sum(np.exp(-(x - k)**2 * k) / k**alpha for k in range(1, n+1))
+
+def metropolis(f,x0=5,n=700_000, sigma=0.5):
+    """Tries to sample a distribution with density function proportional to the function `f`, which needs not to be normalized.
+    The initial condition `x0` may not appear in the resulting array.
+    The `sigma` argument is used as the standard deviation of the perturbation on each step.
+    The size of this sigma should depend on the charactersitic size of the dunction to be sampled.
+
+    Returns an array of length `n` with random samples from `f`."""
+    samples = np.zeros(n)
+    samples[-1] = x0
+    for i in range(n):
+        sample_new = samples[i-1] + random.normal(0,sigma)
+        if random.random() < f(sample_new)/f(samples[i-1]):
+            samples[i] = sample_new
+        else:
+            samples[i] = samples[i-1]
+    return samples
+
+
+# Generar muestras
+samples = metropolis(g)
+
+# Crear histograma
+plt.figure(figsize=(10, 5))
+plt.hist(samples, bins=200, density=True, alpha=0.75, color='blue')
+plt.xlabel('x')
+plt.ylabel('Densidad de probabilidad')
+plt.title('Histograma de muestras generadas con Metrópolis')
+plt.grid()
+plt.savefig("Tarea4/1.a.pdf")
+
+# Calcular A mediante la estimación con f(x) = exp(-x^2) lo vamos a deplazar 4 unidades a la derecha para que
+#tenga pesos similares para todos los valores
+def f(x):
+    return np.exp(-(x-4)**2)
+
+sqrt_pi = np.sqrt(np.pi)
+
+#algebra y definiendo variables para que sea igual que la ecuacion en el pdf
+ratios = f(samples) / g(samples)
+prom = np.sum(ratios) / len(samples)
+A_est = sqrt_pi/(prom)
+
+#propagacion de error
+std_A = A_est * np.sqrt(np.var(ratios)/len(samples)) / prom
+
+# Imprimir resultado
+print(f"1.b) {A_est} ± {std_A}")
+
 '''Ejercicio 2: Integral de camino para difracción de Fresnel'''
 
 # Parámetros
